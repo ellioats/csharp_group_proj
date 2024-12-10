ecfabb4cafbaa4852dcdf8a8810a7fd914ae49bc
Hammad Babar <hammadbabar2000@outlook.com>
2024-12-10 17:10:54 -0500

    GUI 90% Done

--- GroupProjectCSharp/MainWindow.xaml.cs
+++ GroupProjectCSharp/MainWindow.xaml.cs
@@ -1,24 +1,63 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace GroupProjectCSharp
+namespace ArrayTraveler
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private string[,] array;
+
+        public MainWindow(int number1, int number2)
         {
             InitializeComponent();
+            array = new string[number1, number2];
+            InitializeArray();
+            PopulateGrid();
+        }
+
+        private void InitializeArray()
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = "Bitch";
+                }
+            }
+        }
+
+        private void PopulateGrid()
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                ArrayGrid.RowDefinitions.Add(new RowDefinition());
+            }
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                ArrayGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    var cellBorder = new Border
+                    {
+                        BorderBrush = Brushes.Black,
+                        BorderThickness = new Thickness(1),
+                        Child = new TextBlock
+                        {
+                            Text = array[i, j].ToString(),
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            VerticalAlignment = VerticalAlignment.Center
+                        }
+                    };
+                    Grid.SetRow(cellBorder, i);
+                    Grid.SetColumn(cellBorder, j);
+                    ArrayGrid.Children.Add(cellBorder);
+                }
+            }
         }
     }
 }
---
+++ GroupProjectCSharp/StartPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using ArrayTraveler;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GroupProjectCSharp
+{
+    public partial class StartPage : Window
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox != null && (textBox.Text == "Enter first integer" || textBox.Text == "Enter second integer"))
+            {
+                textBox.Text = string.Empty;
+            }
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(TextBox1.Text, out int number1) && int.TryParse(TextBox2.Text, out int number2))
+            {
+                MainWindow mainWindow = new MainWindow(number1, number2);
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid integers in both text boxes.");
+            }
+        }
+    }
+}
