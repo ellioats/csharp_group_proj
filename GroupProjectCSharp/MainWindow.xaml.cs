68c2e2532d1c4e336c398a032910a76f560ba628
ellioats <emwcurrie@gmail.com>
2024-12-10 17:03:25 -0500

    beginning of dll logic

--- GroupProjectCSharp/MainWindow.xaml.cs
+++ GroupProjectCSharp/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HighlanderMovements;
 
 namespace GroupProjectCSharp
 {
@@ -19,6 +20,33 @@ namespace GroupProjectCSharp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            // temporary testing function for HighlanderMovements component
+
+            string[][] area = { 
+                ["", "8", "7", "6", "", ""], 
+                ["", "1", "P", "5", "", ""],
+                ["", "2", "3", "4", "", ""], 
+                ["", "", "", "", "", ""], 
+                ["", "", "", "", "", ""], 
+                ["", "", "", "", "", ""] 
+            };
+
+            int CurrentPlayerX = 2;
+            int CurrentPlayerY = 2;
+
+            Boolean ?isEvil = null;
+
+            string outputText = "";
+
+
+
+            HighlanderMovements.Class1.isHighlanderNear(ref area, CurrentPlayerX, CurrentPlayerY, ref isEvil, ref outputText);
+
+            textBlock.Text = outputText;
         }
     }
 }
