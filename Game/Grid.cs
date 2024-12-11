3c413fa39b1e7c48638311fbb9f92c426e195dcd
ellioats <emwcurrie@gmail.com>
2024-12-11 14:17:27 -0500

    Added gridUpdate()

--- Game/Grid.cs
+++ Game/Grid.cs
@@ -16,6 +16,8 @@ namespace Game
         // contains players on current grid
         private List<HighlanderMovements.Highlander> currentPlayers = new List<Highlander>();
 
+        private string[][] grid;
+
 
         public Grid(int x, int y)
         {
@@ -30,11 +32,32 @@ namespace Game
             this.currentPlayers = playerList;
         }
 
+        // needs testing
+        private void initGrid(int x, int y)
+        {
+            for (int i = 0; i < x; i++)
+            {
+                this.grid[i] = new string[y];
+            }
+        }
+
         List<Highlander> getCurrentPlayers()
         {
             return this.currentPlayers;
         }
 
-
+        public void printGrid()
+        {
+            Console.WriteLine("Grid print");
+            for (int i = 0; i < this.grid.GetLength(0); i++)
+            {
+                for (int ii = 0; i < this.grid.GetLength(1); ii++)
+                {
+                    Console.Write("{0}, ", this.grid[i][ii]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n");
+        }
     }
 }
