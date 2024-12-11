f4410bf0e1a9b516abb7be6d8e3ee64a6f015810
ellioats <emwcurrie@gmail.com>
2024-12-10 23:37:11 -0500

    prep for merge

--- GroupProjectCSharp/obj/Debug/net8.0-windows/GroupProjectCSharp.AssemblyInfo.cs
+++ GroupProjectCSharp/obj/Debug/net8.0-windows/GroupProjectCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GroupProjectCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5d13e5def023332a5c025bbfb849c1c8d0cbb5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfba80ef9137690e67099b11102bab527aab4713")]
 [assembly: System.Reflection.AssemblyProductAttribute("GroupProjectCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GroupProjectCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
