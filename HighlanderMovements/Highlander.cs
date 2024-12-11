edab4860abc5fc0d9f84433f1b2d49022f2083f2
ellioats <emwcurrie@gmail.com>
2024-12-11 17:16:47 -0500

    added array size boundary detection for random movement logic

--- HighlanderMovements/Highlander.cs
+++ HighlanderMovements/Highlander.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -26,6 +28,7 @@ namespace HighlanderMovements
         public int prevY { get; private set; }
 
         private Boolean setPrevCoordPace = false;
+        private Random rand;
 
         public Highlander(int startX, int startY, bool isGood)
         {
@@ -38,6 +41,8 @@ namespace HighlanderMovements
             
 
             IsGood = isGood;
+
+            rand = new Random();
         }
 
         // when a highlander kills another highlander, run this 
@@ -60,6 +65,63 @@ namespace HighlanderMovements
                 this.prevX = this.X;
                 this.prevY = this.Y;
             }
+
+        }
+
+        public void MoveRandomly(int maxWidth, int maxHeight)
+        {
+            int direction = rand.Next(0, 8);
+
+            switch (direction) {
+                case 0:
+                    MoveRight();
+                    break;
+                case 1:
+                    MoveDownRight();
+                    break;
+                case 2:
+                    MoveDown();
+                    break;
+                case 3:
+                    MoveDownLeft();
+                    break;
+                case 4:
+                    MoveLeft();
+                    break;
+                case 5:
+                    MoveUpLeft();
+                    break;
+                case 6:
+                    MoveUp();
+                    break;
+                case 7:
+                    MoveUpRight();
+                    break;
+            }
+
+            //Console.WriteLine("validating position [{0},{1}]", X, Y);
+            this.validateNewPosition(maxWidth, maxHeight);
+
+        }
+
+        private void validateNewPosition(int maxWidth, int maxHeight)
+        {
+            // revert to prevX or prevY values
+            if (this.X == maxWidth || this.X == -1)
+            {
+                //Console.WriteLine("{0} is equal to {1}(maxWidth)", X, maxWidth);
+                this.X = (X < 0) ? 1 : maxWidth - 1 ;
+                //this.MoveRandomly(maxWidth, maxHeight);
+            } else if (this.Y == maxHeight || this.Y == -1)
+            {
+                //Console.WriteLine("{0} is equal to {1}(maxHeight)", Y, maxHeight);
+                this.Y = (Y < 0) ? 1 : maxHeight - 1 ;
+                //this.MoveRandomly(maxWidth, maxHeight);
+            }
+
+            //Console.WriteLine("Returning...");
+            return;
+
 
         }
 
