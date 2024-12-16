4bf063355870b41540171672e72a102551048bab
ellioats <emwcurrie@gmail.com>
2024-12-16 08:51:13 -0500

    assembly changes:

--- GroupProjectCSharp/obj/Debug/net8.0-windows/GroupProjectCSharp.AssemblyInfo.cs
+++ GroupProjectCSharp/obj/Debug/net8.0-windows/GroupProjectCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GroupProjectCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72cfa1a349e8aff79e39a3dd7484874cd28802f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50498635350e7b490242ef1d9f1b0e30800e8915")]
 [assembly: System.Reflection.AssemblyProductAttribute("GroupProjectCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GroupProjectCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
